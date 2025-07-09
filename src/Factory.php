c863a3eb62479f42783fff4b25ae2a83140dbcb9
martinyde <yde001@gmail.com>
2025-07-09 09:08:31 +0200

    Added type declarations to factory class

--- src/Factory.php
+++ src/Factory.php
@@ -4,16 +4,18 @@ namespace WordSearch;
 
 class Factory
 {
-    /**
-     * Create a puzzle.
-     *
-     * @param array   $words List of words.
-     * @param integer $size  Grid size.
-     * @param string  $lang  Language.
-     * @return Puzzle
-     */
-    public static function create(array $words, $size = 15, $lang = 'en')
-    {
+
+  /**
+   * Create a puzzle.
+   *
+   * @param array $words List of words.
+   * @param integer $size Grid size.
+   * @param string $lang Language.
+   *
+   * @return Puzzle
+   * @throws \WordSearch\Exception
+   */
+    public static function create(array $words, int $size = 15, $lang = 'en'): Puzzle {
         $alphabet = ($lang == 'fi') ? new Alphabet\Finnish() : null;
         $generator = new Generator($words, $size, $alphabet);
         return $generator->generate();
